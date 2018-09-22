806514befa95b4448acf18c53735ce5d8f96c2f9
Aakash Verma <aakashvermaee@gmail.com>
2018-09-22 14:44:04 +0530

    M index.js route parsing

--- index.js
+++ index.js
@@ -3,12 +3,30 @@ require("./config");
 const port = process.env.PORT || 2018;
 
 const http = global.getPackage("http");
+const url = global.getPackage("url");
 
 const server = http.createServer((req, res) => {
-  res.write("Welcome");
-  res.end();
+  routeApi(req, res);
 });
 
 server.listen(port, () => {
   process.logger(`App running on port: ${port}`);
 });
+
+const routeApi = (req, res) => {
+  const uri = url
+    .parse(req.url)
+    .pathname;
+  process.logger(uri);
+
+  switch (uri) {
+  case "/":
+    res.write("Welcome");
+    res.end();
+    break;
+  case "/login":
+    res.write("login");
+    res.end();
+    break;
+  }
+};
